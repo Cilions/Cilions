6d734328e40544adfc71756a13c36c5175bdc4f2
Cilions <denizkaratov@gmail.com>
2023-06-18 11:52:31 +0600

    ✏️

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import Head from 'next/head'
 import Link from 'next/link'
 import useSWR from 'swr'
 
-const fetcher = (...args: Parameters<typeof fetch>) => 
-    fetch(...args).then((res) => res.json());
+const fetcher = (...args: Parameters<typeof fetch>) =>
+  fetch(...args).then(res => res.json())
 
 const Home: NextPage = () => {
   const { data, error } = useSWR(
@@ -18,46 +18,26 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>{data.login} ~ {data.bio}</title>
+        <title>
+          {data.login} ~ {data.bio}
+        </title>
       </Head>
 
       {/* v0.2 */}
 
-      <pre>{data.login} ~ {data.bio}</pre>
+      <pre>
+        {data.login} ~ {data.bio}
+      </pre>
 
       <pre style={{ margin: '2rem 0' }}>✿✿✿✿✿✿✿✿✿✿✿✿✿✿✿✿</pre>
 
       <div style={{ margin: '2rem 0' }}>
-
-      <pre>Inst: (private)</pre>
-        <Link href="https://instagram.com/_cilions">
-          <a>
-            <pre>instagram.com/_cilions</pre>
-          </a>
-        </Link>
-
-        <pre>Twitter: (private)</pre>
+        <pre>Twitter:</pre>
         <Link href="https://twitter.com/Cilions_">
           <a>
             <pre>twitter.com/Cilions_</pre>
           </a>
         </Link>
-
-        <pre>Spotify:</pre>
-        <Link href="https://open.spotify.com/user/2gtgl72ljc9zuop4gun3d6qih">
-          <a>
-            <pre>spotify.com/user/2gtgl72ljc9zuop4gun3d6qih</pre>
-          </a>
-        </Link>
-
-        <pre>Discord:</pre>
-        <Link href="https://discordapp.com/users/608740202491674634">
-          <a>
-            <pre>discordapp.com/users/608740202491674634</pre>
-          </a>
-        </Link>
-
-        <pre style={{ margin: '2rem 0' }}>๑ ⋆˚₊⋆─────────────⋆˚₊⋆ ๑</pre>
 
         <pre>Github:</pre>
         <Link href="https://github.com/Cilions">
@@ -66,17 +46,10 @@ const Home: NextPage = () => {
           </a>
         </Link>
 
-        <pre>LinkedIn:</pre>
-        <Link href="https://linkedin.com/in/cilions">
+        <pre>Instagram:</pre>
+        <Link href="https://instagram.com/_cilions">
           <a>
-            <pre>linkedin.com/in/cilions</pre>
-          </a>
-        </Link>
-
-        <pre>Google Developers: (private)</pre>
-        <Link href="https://g.dev/clion">
-          <a>
-            <pre>g.dev/clion</pre>
+            <pre>instagram.com/_cilions</pre>
           </a>
         </Link>
       </div>
@@ -86,7 +59,19 @@ const Home: NextPage = () => {
         <pre>cilions@pm.me</pre>
       </a>
 
-      <pre style={{ margin: '2rem 0' }}>v0.2.4 ~ @cilions ❤️</pre>
+      <pre style={{ margin: '2rem 0' }}></pre>
+
+      <Link href="https://github.com/Cilions/my-app">
+        <a>
+          <pre>Source code</pre>
+        </a>
+      </Link>
+      <Link href="https://app.splitbee.io/public/cilions.icu">
+        <a>
+          <pre>Traffic analytics</pre>
+        </a>
+      </Link>
+      <pre style={{ margin: '2rem 0' }}>v0.2.6 ~ @cilions ❤️</pre>
     </>
   )
 }
